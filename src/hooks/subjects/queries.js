fcc3cb9d01de80b885f000939de5d49e25f59ec7
Maksym Habinskyi <mgabin348@gmail.com>
2025-12-19 06:01:07 +0100

    That's a lot of damage

--- src/hooks/subjects/queries.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import * as subjectsAPI from "../../api/subjectsAPI.js";
-
-export function useSubjects() {
-  return useQuery({
-    queryKey: ["subjects"],
-    queryFn: subjectsAPI.getAllSubjects,
-  });
-}
